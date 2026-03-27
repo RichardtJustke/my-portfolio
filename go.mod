d1dff9ec46dbde568b50912c6fcdcb6019206e88
RichardtJustke <rj.justke@gmail.com>
2026-03-27 11:59:58 -0300

    pingpong

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RichardtJustke/my-portfolio
+
+go 1.26.1
--- main.go
+++ main.go
@@ -3,28 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-
-	"github.com/richardtjustke/site/handlers"
 )
 
 func main() {
-	r := chi.NewRouter()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("oi mundo"))
+	})
 
-	// Middleware: loga cada requisição e recupera de panics
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	// Arquivos estáticos (CSS, JS)
-	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-	// Rotas
-	r.Get("/", handlers.Home)
-	r.Get("/writing", handlers.WritingList)
-	r.Get("/writing/{slug}", handlers.WritingPost)
-
-	log.Println("servidor rodando em http://localhost:3000")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	log.Println("rodando em http://localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
