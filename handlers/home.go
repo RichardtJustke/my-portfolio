1264d8e38a89054ae605f7b6b6428cd50c887954
richardtjustke <rj.justke@gmail.com>
2026-03-31 05:43:43 -0300

    att

--- handlers/home.go
+++ handlers/home.go
@@ -1,25 +1,25 @@
 package handlers
 
 import (
-    "html/template"
-    "net/http"
+	"html/template"
+	"net/http"
 )
 
 var tmpl *template.Template
 
 func SetTemplates(t *template.Template) {
-    tmpl = t
+	tmpl = t
 }
 
 type HomeData struct {
-    Name     string
-    Role     string
-    Bio      string
-    Location string
-    Email    string
-    GitHub   string
-    LinkedIn string
-    Page     string
+	Name     string
+	Role     string
+	Bio      string
+	Location string
+	Email    string
+	GitHub   string
+	LinkedIn string
+	Page     string
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
